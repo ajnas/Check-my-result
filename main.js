5291d36f9b5675ce6c738581b36d0bbebf5849eb
Ajnas <mailajnaskt@gmail.com>
2014-06-18 21:01:26 +0530

    Adding notification

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var request = require('request')
   , cheerio = require('cheerio');
 
+var growl = require('growl');
 
 var url = 'http://keralaresults.nic.in/'
 var count=-1;
@@ -11,8 +12,14 @@ request(url, function(err, resp, body){
   $ = cheerio.load(body);
   links = $('a'); //use your CSS selector here
   var new_count=links.length
-  if(count>=0 && new_count!=count)
-  		console.log("Results out");
+  if(count==-1)
+  		count=new_count
+
+  if(new_count!=count)
+  		{
+  		 growl('Seems like results are out !');
+  		}
+
   check_count++;
   console.log("Number of times :"+check_count)
   
